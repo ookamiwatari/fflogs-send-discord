5d6cb59aba8fd6b5611765ed029d2b4a7b15d42b
ookamiwatari <ookamiwatari@gmail.com>
2017-04-30 08:31:26 +0900

    Fix: phantomjsのテスト実行において引数の指定が間違えていた問題を修正

--- test.js
+++ test.js
@@ -5,16 +5,15 @@ var binPath = phantomjs.path
  
 var childArgs = [
   path.join(__dirname, 'phantomjs-script.js'),
-  '--webdriver-loglevel=ERROR',
-  'https://www.fflogs.com/reports/',
+  'https://ja.fflogs.com/reports/',
   'PmYRaMwkT1vcpZh6',
-  '6'
+  '33'
 ];
 
 childProcess.execFile(binPath, childArgs, function(err, stdout, stderr) {
   console.log("start");
   //var response = stdout.match(/.*Start_Response\s+([\s\S]*)\s+End_Response.*/)[1].slice(0,-1);
   //response = response.substr(0,response-1);
-  console.log(stdout.match(/.*Start_Response\s+([\s\S]*)\s+End_Response.*/)[1].slice(0,-2));
+  console.log(stdout.match(/.*Start_Response\s+([\s\S]*)\s?End_Response.*/)[1].slice(0,-2));
   console.log("end");
 })
